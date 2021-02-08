898a274d485209ff38bbe6a6083b576b9e0c34bc
Sara Nielsen <sara345a@stud.kea.dk>
2021-02-08 17:11:07 +0100

    1-8 option works, still missing to show in output

--- script.js
+++ script.js
@@ -1,37 +1,79 @@
 "use strict"
 
-window.addEventListener("load", start);
+window.addEventListener("DOMContentLoaded", start);
 
 //Start
-function start(){
+function start() {
     console.log("start");
-
-    document.querySelector("#clear").addEventListener("click", clear);
-
-    clickGenOutput();
+    document.querySelector("#genoutput").addEventListener("click", readOptionInput);
 }
 
-//Make generate clickable
-function clickGenOutput(){
-    console.log("clickGenOutput");
+function readOptionInput() {
+    const readInput = document.querySelector("#input").value;
+    const operatorInput = document.querySelector("#operator");
 
-    document.querySelector("#genoutput").addEventListener("click", genOutput);
+
+ const firstSpace = readInput.indexOf(" ");
+ const lastSpace = readInput.lastIndexOf(" ");
+
+ const lastPng = readInput.lastIndexOf(".");
+ const lastJpg = readInput.lastIndexOf(".");
+ const findPng = readInput.substring(lastPng + 1);
+ const findJpg = readInput.substring(lastJpg + 1);
+
+ const star = "*";
+
+const replace = readInput.substring(firstSpace + 1);
+
+ let finalOutput = "";
+   //if-statements
+   if (operatorInput.value === "opt1") {
+    finalOutput = readInput.substring(0, 1).toUpperCase() + readInput.substring(1);
+    console.log(finalOutput);
+    showOutput();
+   }else if (operatorInput.value==="opt2") {
+    finalOutput = readInput.substring(0, firstSpace);
+    showOutput();
+    console.log(finalOutput);
+   }else if (operatorInput.value === "opt3") {
+       finalOutput = readInput.substring(0, firstSpace).length;
+       console.log(finalOutput);
+       showOutput();
+   }else if (operatorInput.value === "opt4") {
+       finalOutput = readInput.substring(firstSpace + 1, lastSpace).trim();
+       console.log(finalOutput);
+       showOutput();
+   }else if (operatorInput.value==="opt5") {
+       if (findJpg === "jpg" || findPng === "png") {
+           console.log("true");
+           finalOutput = readInput;
+           console.log(finalOutput);
+       }else {
+        console.log("false");
+        finalOutput = "ERROR! use a filename!";
+          }
+        showOutput();
+   }else if (operatorInput.value === "opt6") {
+       finalOutput = star.repeat(readInput.length);
+       console.log(finalOutput);
+       showOutput();
+   }else if (operatorInput.value === "opt7") {
+       const thing1 = readInput.substring(0,2).toLowerCase();
+       const thing2 = readInput[2].toUpperCase();
+       const thing3 = readInput.substring(3).toLowerCase();
+
+       finalOutput = thing1 + thing2 + thing3;
+       console.log(finalOutput);
+       showOutput();
+   }
+ else {
+       finalOutput = readInput.substring(0, 1).toUpperCase() + readInput.substring(1);
+       console.log(finalOutput);
+       showOutput();
+   }
 }
 
-function readInput(){
-    console.log("readInput");
-
-    let input
-}
-
-function genOutput(){
-    console.log("generateOutput");
-
-//Skal hente input fra 'readInput' via 'return'
-
-}
-
-function clear() {
-    console.log("clearOutput");
-    document.querySelector("#output").innerHTML = "";
+function showOutput() {
+    let finalOutput = "";
+    document.querySelector("#output").value = finalOutput.toString();
 }
